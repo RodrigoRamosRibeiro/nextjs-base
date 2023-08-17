bdb6ba6d73a35f7368a8680181c1ce8e3ee64551
Rodrigo <rrrodrigoribeirorrr@gmail.com>
2023-08-17 23:11:29 +0100

    initial

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function Home() {
+  return (
+    <>
+      <h1 className="flex flex-col  text-[10rem] text-black ">ok tudo bem</h1>
+      <div>olas</div>
+    </>
+  );
+}
+export default Home;
